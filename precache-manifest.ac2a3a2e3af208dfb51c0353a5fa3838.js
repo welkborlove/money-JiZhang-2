9e6d7309fb7956a1bdd963a30965d183989ce527
wekbor <364102504@qq.com>
2020-09-18 19:46:23 +0800

    deploy

---
+++ precache-manifest.ac2a3a2e3af208dfb51c0353a5fa3838.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a088249f3f907a9bb3",
+    "url": "/money-JiZhang-2/css/app.f79cfaa7.css"
+  },
+  {
+    "revision": "71e86699a5c591f8e39f33ebda5a1f8b",
+    "url": "/money-JiZhang-2/index.html"
+  },
+  {
+    "revision": "f6a088249f3f907a9bb3",
+    "url": "/money-JiZhang-2/js/app.34f0987f.js"
+  },
+  {
+    "revision": "367d85473b0d5a249ff6",
+    "url": "/money-JiZhang-2/js/chunk-vendors.72853ade.js"
+  },
+  {
+    "revision": "42bfa4ba9edfb3426e53b3524294dc7f",
+    "url": "/money-JiZhang-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-JiZhang-2/robots.txt"
+  }
+]);
